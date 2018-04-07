62e80a95a14c963836eb2297956591efc2f46488
MaoLifeng <396621081@qq.com>
2018-04-07 16:54:18 +0800

    payment

--- src/page/result/index.js
+++ src/page/result/index.js
@@ -6,5 +6,10 @@ var _mm = require('util/mm.js');
 $(function(){
   var type = _mm.getUrlParam('type') || 'default',
       $element = $('.' + type + '-success');
+  if(type === 'payment') {
+    var orderNumber= _mm.getUrlParam('orderNumber'),
+        $orderNo = $element.find('.order-number');
+    $orderNo.attr('href',$orderNo.attr('href')+orderNumber);
+  }
   $element.show();
 })
